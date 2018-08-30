74195d7b74805a59b83db25882d3d67c02b4730d
HankBedford <Zachary.Lynch1337@gmail.com>
2018-08-30 15:52:48 -0700

    Trying out some cool features in react like creating my own components

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,20 +1,28 @@
+// remember about hacker haze theme on vs
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hello Alli!</h1>
+        <p>Can nest JSX elements, but cannot really have more than one parent element</p>
+        <Person />
+        <Person />
+        <Person />
+        <Person />
+        <Person />
       </div>
     );
+
+    // code below is the equivilant of the code above, the code below gets
+    // compiled into the code above
+    // code below is not recommended, code above is
+    // return React.createElement('div', {className: 'App'}, 
+    // React.createElement('h1', null, 'Hi baby!!'));
   }
 }
 
---
+++ first-app/src/Person/Person.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+const person = () => {
+    return (
+        <p>I, was once, a man</p>
+    );
+}
+
+export default person;
