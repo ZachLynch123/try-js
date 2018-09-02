cedd2fd22c0d6a440fb05d5517ba82075f5e4420
HankBedford <Zachary.Lynch1337@gmail.com>
2018-09-01 22:23:28 -0700

    Passed method as a prop to Person.js that does not have access to state

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -13,18 +13,26 @@ class App extends Component {
       {name: 'Zach', age: 23},
       {name: 'Alli', age: 20},
       {name: 'Matt', age: 25},
-      {name: 'Danny'}
     ]
   }
 
   // usually use Handler at the end of method in order to 
   // show that this method is an event handler (like clicks or something)
   switchNameHandler = () => {
-    console.log('Was Clicked');
+    //console.log('Was Clicked');
     /* cannot call 'this' because it will not refer to the class 'this'
     given the nature of ES5 js. By using the ES6 syntax here, we work around the issue
-
+    so using 'this' will work in this case
     */
+
+   // DON'T DO THIS, USE SPECIAL METHOD setState this.state.persons[0].name="zekahri"
+    this.setState({
+      persons: [
+        {name: 'Zachary', age: 23},
+        {name: 'Allison', age: 20},
+        {name: 'Matthew', age: 25},
+        {name: 'Danny', age: 23}
+       ]})
   }
 
 
@@ -35,11 +43,17 @@ class App extends Component {
         <p>Can nest JSX elements, but cannot really have more than one parent element</p>
         {/* do not pass method with parentheses, that will execute the method at runtime not when clicked */}
         <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My hobbies: Racing
+        <Person
+        name={this.state.persons[0].name} 
+        age={this.state.persons[0].age} />
+        <Person 
+        name={this.state.persons[1].name} 
+        age={this.state.persons[1].age} />
+        <Person
+        name={this.state.persons[2].name} 
+        age={this.state.persons[2].age} click={this.switchNameHandler}>
+        My hobbies: Racing
         </Person>
-        <Person name={this.state.persons[3].name}></Person>
       </div>
     );
 
--- first-app/src/Person/Person.js
+++ first-app/src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 const person = (props) => {
     return (
         <div>
-            <p>I am {props.name} and I'm {props.age}</p>
+            <p onClick={props.click}>I am {props.name} and I'm {props.age}</p>
             <p>{props.children}</p>
         </div>
     );
